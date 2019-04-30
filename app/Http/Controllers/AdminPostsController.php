429934fe8b273707e7b592670f73850678a7aecf
odahara178 <odahara178@gmail.com>
2019-04-30 14:50:53 +0000

    カテゴリポスト作成

--- app/Http/Controllers/AdminPostsController.php
+++ app/Http/Controllers/AdminPostsController.php
@@ -9,6 +9,7 @@ use App\Post;
 use Illuminate\Support\Facades\Auth;
 use App\Photo;
 use App\User;
+use App\Category;
 
 class AdminPostsController extends Controller
 {
@@ -31,7 +32,11 @@ class AdminPostsController extends Controller
      */
     public function create()
     {
-        return view('admin.posts.create');
+
+        $categories = Category::lists('name', 'id')->all();
+
+
+        return view('admin.posts.create', compact('categories'));
     }
 
     /**
